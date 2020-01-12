cca8b2a6601c2333e4c866187ce999895122f3ee
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-01-12 16:01:37 +0100

    Add files via upload

---
+++ HazzardWifi/HazzardWifi.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "IoT";
+
+const char* pass = "IoT2018!";
+
+void setup() {
+
+  Serial.begin(115200);
+
+  WiFi.begin(ssid,pass);
+
+  while(WiFi.status() != WL_CONNECTED) {
+
+    Serial.print(".");
+
+    delay(1000);
+
+  }
+
+  Serial.println("Uppkopplad");
+
+}
+
+void loop() {
+
+}
